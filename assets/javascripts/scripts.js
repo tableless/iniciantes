af5d0219c22840ddd189ed18985695c734223648
scard69 <vit.scard@gmail.com>
2017-03-23 23:54:15 -0300

    Teste (#120)
    * Texto inicial para Estruturas de Controle e Laços
    
    * Ajustes no texto
    
    * Palavra corrigida
    
    Trocando de "largura" para "altura" no cálculo da altura.
    
    * Correção no texto
    
    * Typo Correction
    
    * Update index.html
    
    * Update index.html
    
    * Começando a escrever
    
    * Início da escrita de operadores aritiméticos
    
    * Mais exemplos operações básicas e inicio dos outros operadores aritiméticos
    
    * Final operadores aritiméticos
    
    * Finalização operadores aritiméticos
    
    * Abrir link externo em outra página
    
    * Metatags - Correção de html. Existem dt's e dd's, mas não existe uma dl envelopando tudo. Coloquei a dl
    
    * Remoção ultimo parágrafo e precedência de nome/operador alterada para operador/nome
    
    * Correção do Igual Estrito (===)
    
    De acordo com a [Issue](https://github.com/tableless/iniciantes/issues/74#issuecomment-157571608) aberta por [olinad0](https://github.com/olinad0)
    
    * Update font.html
    
    Adicionado font family
    
    * doctype e ortografia
    
    * Adiciona o texto inicial sobre o que é DOM

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
   prettyPrint();
+
+  // Altera links externos para abrirem em uma nova aba
+  var domain = document.location.protocol+'//'+document.location.host,
+    links = $('.content a').each(function(index,element) {
+    if(element.href.substr(0,domain.length) !== domain) {
+      element.target = '_blank';
+    }
+  });
 });
